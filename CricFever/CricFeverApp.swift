35389d2c3fec2f1a4490d4f9b0af98a2047d0191
Alwin Vk <alwinvarghesek@gmail.com>
2022-12-09 20:22:05 +0000

    Initial Commit

---
+++ CricFever/CricFeverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricFeverApp.swift
+//  CricFever
+//
+//  Created by Alwin VK on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CricFeverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
